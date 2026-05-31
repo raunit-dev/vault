7778e1b4d2d477a7fd6cdeb47b371731ab5aa446
Raunit Jaiswal <raunitjaiswal510@gmail.com>
2026-05-31 16:45:58 +0530

    Add update nav negative tests

--- integration-tests/src/async_vault/update_vault_nav.rs
+++ integration-tests/src/async_vault/update_vault_nav.rs
@@ -1,10 +1,11 @@
 use anchor_spl::token;
 use async_vault_client::{lite::SendTransaction, sdk::program_id, UpdateVaultNavBuilder, Vault};
+use borsh::BorshSerialize;
 use litesvm::LiteSVM;
-use solana_sdk::{account::ReadableAccount, signer::Signer};
+use solana_sdk::{account::ReadableAccount, signature::Keypair, signer::Signer};
 use test_case::test_case;
 
-use crate::async_helper_functions::set_up_async_vault;
+use crate::async_helper_functions::{assert_error_code, set_up_async_vault};
 
 #[test_case(200 ; "update nav succeeds")]
 #[test_case(0 ; "update nav to zero succeeds")]
@@ -53,4 +54,79 @@ fn test_update_vault_nav(updated_nav: u128) {
 
     assert_eq!(vault_config.nav, updated_nav);
     assert_eq!(vault_config.nav_version, nav_version_before + 1);
+}
+
+#[test]
+fn test_update_vault_nav_unauthorized_signer_fails() {
+    let mut svm = LiteSVM::new();
+    let program_bytes = include_bytes!("../../../target/deploy/async_vault.so");
+    svm.add_program(program_id(), program_bytes).unwrap();
+
+    let (
+        _authority,
+        _payer,
+        _mint_authority,
+        _asset_mint,
+        _share_mint,
+        _user,
+        _operator,
+        _fee_recipient,
+        _reserve_pubkey,
+        vault_pubkey,
+        _pending_vault_pubkey,
+        _fee_recipient_ata,
+        _user_share_account,
+    ) = set_up_async_vault(&mut svm, token::ID, None, token::ID, 1_000_000_000);
+
+    let unauthorized = Keypair::new();
+    svm.airdrop(&unauthorized.pubkey(), 1_000_000_000).unwrap();
+
+    let result = UpdateVaultNavBuilder::new()
+        .authority(unauthorized.pubkey())
+        .vault(vault_pubkey)
+        .updated_nav(200)
+        .instruction()
+        .send_transaction(&mut svm, &unauthorized.pubkey(), &[&unauthorized]);
+
+    assert_error_code(&result.unwrap_err(), 6001, "UnauthorizedSigner");
+}
+
+#[test]
+fn test_update_vault_nav_version_overflow_fails() {
+    let mut svm = LiteSVM::new();
+    let program_bytes = include_bytes!("../../../target/deploy/async_vault.so");
+    svm.add_program(program_id(), program_bytes).unwrap();
+
+    let (
+        authority,
+        _payer,
+        _mint_authority,
+        _asset_mint,
+        _share_mint,
+        _user,
+        _operator,
+        _fee_recipient,
+        _reserve_pubkey,
+        vault_pubkey,
+        _pending_vault_pubkey,
+        _fee_recipient_ata,
+        _user_share_account,
+    ) = set_up_async_vault(&mut svm, token::ID, None, token::ID, 1_000_000_000);
+
+    let mut account = svm.get_account(&vault_pubkey).expect("vault should exist");
+    let mut vault = Vault::from_bytes(account.data()).unwrap();
+    vault.nav_version = u64::MAX;
+    let mut buf = Vec::new();
+    vault.serialize(&mut buf).unwrap();
+    account.data = buf;
+    svm.set_account(vault_pubkey, account).unwrap();
+
+    let result = UpdateVaultNavBuilder::new()
+        .authority(authority.pubkey())
+        .vault(vault_pubkey)
+        .updated_nav(200)
+        .instruction()
+        .send_transaction(&mut svm, &authority.pubkey(), &[&authority]);
+
+    assert_error_code(&result.unwrap_err(), 6002, "ArithmeticError");
 }
