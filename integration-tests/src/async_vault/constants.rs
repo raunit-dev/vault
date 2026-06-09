2d328cdb99e7387d5f8bffc9ab24a3374e83fb83
@runitinrust <raunitjaiswal510@gmail.com>
2026-06-09 19:43:54 +0530

    Add cancel request negative tests (#77)
    * Add cancel request negative tests
    
    * Use shared error constants in cancel request tests

--- integration-tests/src/async_vault/constants.rs
+++ integration-tests/src/async_vault/constants.rs
@@ -1,5 +1,7 @@
 pub(crate) const UNAUTHORIZED_SIGNER: u32 = 6001;
+pub(crate) const PAUSED_VAULT: u32 = 6003;
 pub(crate) const ARITHMETIC_ERROR: u32 = 6009;
 pub(crate) const REQUEST_INVALID_STATE: u32 = 6017;
+pub(crate) const REQUEST_IS_NOT_PENDING: u32 = 6018;
 pub(crate) const MISSING_REQUIRED_ACCOUNT: u32 = 6020;
 pub(crate) const APPROVAL_REQUEST_MISMATCH: u32 = 6038;
